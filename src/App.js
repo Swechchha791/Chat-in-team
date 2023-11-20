b91bf6db7fa525fb72367ed1969946448f4bd99a
Swechchha <swechchhapandey271@gmail.com>
2023-11-20 23:06:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'd3ec8b6b-5875-40a9-bd6c-51638e54e7ea';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App;
